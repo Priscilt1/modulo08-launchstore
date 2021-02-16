5a80cb36ad92d1c516bd8740da695fa9cd382713
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-02-16 11:33:33 -0300

    formulario de registro

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,17 +1,5 @@
-// entendendo sobre heranca 
-
-class Person {
-    getName () {
-        return this.name
+module.exports = {
+    registerForm(req, res) {
+        return res.render('user/register')
     }
 }
-
-class Dev extends Person {
-    constructor(name) {
-        super() //o super pega tudo da classe
-        this.name = name 
-    }
-}
-
-const dev = new Dev ("diego")
-console.log(dev.getName())
