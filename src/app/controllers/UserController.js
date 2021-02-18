adc2f4e96df1cd48aa9c4421a6220c0d68ebfe45
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-02-18 18:24:14 -0300

    corrigindo bugs

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,7 +4,7 @@ module.exports = {
     registerForm(req, res) {
         return res.render('user/register')
     },
-    post(req, res) {
+    async post(req, res) {
         //verificar se todos os campos estao preenchifos
         const keys = Object.keys(req.body)
 
@@ -16,13 +16,20 @@ module.exports = {
 
 
         //verificar se usuario já existe (pelo o email e cpf/cnpj)
-        const {email, cpf_cnpj} = req.body
-        const user = await User.findOne({ //passando o filtro como objeto
+        const {email, cpf_cnpj, password, passwordRepeat} = req.body
+        //passando o filtro como objeto
+        const user = await User.findOne({ 
             where: {
                 email
             }, or: {cpf_cnpj}
         })
 
+        if(user) return res.send('Esse usuario já existe')
+
         //verificar se as duas senhas estão iguais 
+        if(password != passwordRepeat)
+            return res.send('Coloque a mesma senha')
+
+        return res.send('Passou')
     }
 }
