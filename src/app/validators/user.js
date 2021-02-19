67900acef2ad425193e2e04737a294d8ee32a528
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-02-19 11:04:44 -0300

    refatorando estilos css

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,37 +1,37 @@
 const User = require('../models/User')
 
 async function post(req, res, next) {
-       //verificar se todos os campos estao preenchifos
-       const keys = Object.keys(req.body)
+    //verificar se todos os campos estao preenchifos
+    const keys = Object.keys(req.body)
 
-       for (key of keys) {
-           if (req.body[key] == "") {
-               return res.send('Por favor, preencha todos os campos!')
-           }
-       }
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Por favor, preencha todos os campos!')
+        }
+    }
 
 
-       //verificar se usuario já existe (pelo o email e cpf/cnpj)
-       let {email, cpf_cnpj, password, passwordRepeat} = req.body
+    //verificar se usuario já existe (pelo o email e cpf/cnpj)
+    let {email, cpf_cnpj, password, passwordRepeat} = req.body
 
-       cpf_cnpj = cpf_cnpj.replace(/\D/g, '')
+    cpf_cnpj = cpf_cnpj.replace(/\D/g, '')
 
-       //passando o filtro como objeto
-       const user = await User.findOne({ 
-           where: { email}, 
-           or: {cpf_cnpj}
-       })
+    //passando o filtro como objeto
+    const user = await User.findOne({ 
+        where: { email}, 
+        or: {cpf_cnpj}
+    })
 
-       if(user) return res.render('user/register', {
-           user: req.body, //para quando der errp, as informçÕes serem mantidas no input para o usuario nao precisar digitar tudo novamente
-           error: 'Usuário já cadastrado'
-       })
+    if(user) return res.render('user/register', {
+        user: req.body, //para quando der errp, as informçÕes serem mantidas no input para o usuario nao precisar digitar tudo novamente
+        error: 'Usuário já cadastrado'
+    })
 
-       //verificar se as duas senhas estão iguais 
-       if(password != passwordRepeat)
-           return res.send('Coloque a mesma senha')
+    //verificar se as duas senhas estão iguais 
+    if(password != passwordRepeat)
+        return res.send('Coloque a mesma senha')
 
-       next()
+    next()
 }
 
 module.exports = {
