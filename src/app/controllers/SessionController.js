a3866ecfe642dec261336cd44552097eaa091fdc
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-02-24 10:02:55 -0300

    chave criptografada para pedido de recuperação de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto') // modulo para criação do token
+
 module.exports = {
     loginForm(req, res) {
         return res.render('session/login')
@@ -14,6 +16,23 @@ module.exports = {
         return res.render('session/forgot-password')
     },
     forgot(req, res) {
-        
+        const user = req.user
+
+        //um token para esse usuario
+        const token = crypto.randomBytes(20).toString('hex')
+
+        // criar uma expiração
+        let now = new Date()
+        now = now.setHours(now.getHours() + 1) //1 hora para expirar o token
+
+        await User.update(user.id, {
+            reset_token: token,
+            reset_token_expires:now
+        })
+
+        //enviar um email com um link de recuperação de senha
+
+        // avisar o usuario que enviamos o email
+
     }
 }
