a4d18519b49f93947b54b3ae26a08adcb64a0908
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-01 10:57:40 -0300

    Padrao MVC

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,103 +1,91 @@
-const db = require('../../config/db')
-const {hash} = require('bcryptjs') //pegando a biblioteca para criptografia das senhas
-// const { update } = require('../controllers/UserController')
-const fs = require('fs') //FileSistem para deletar os arquivos do usuario
-const Product = require('../models/Product')
+// const db = require('../../config/db')
+// const {hash} = require('bcryptjs') //pegando a biblioteca para criptografia das senhas
+// const fs = require('fs') //FileSistem para deletar os arquivos do usuario
+// const Product = require('../models/Product')
+const Base = require('./Base')
 
+Base.init ({ table: 'users '}) //iniciando a tabla (pegando no Base)
 
-module.exports = {
-    async findOne(filters) {
-        let query = `SELECT * FROM users`
+const User = {
+    ...Base,
+    // async create(data) {
+    //     try{
+    //         const query = `
+    //         INSERT INTO users (
+    //             name,
+    //             email,
+    //             password, 
+    //             cpf_cnpj,
+    //             cep, 
+    //             address
+    //         ) VALUES ($1, $2, $3, $4, $5, $6)
+    //         RETURNING id
+    //         `
+    //         //hash de senha - criptografia de senha 
+    //         // instalando a biblioteca bcryptjs
+    //         const passwordHash = await hash(data.password, 8) //a força da senha sera 8
 
-        Object.keys(filters).map(key => {
-            //WHERE | OR | AND
-            query = `${query}
-            ${key}
-            `
-            
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            })
-        })
+    //         const values = [
+    //             data.name,
+    //             data.email,
+    //             passwordHash,
+    //             data.cpf_cnpj.replace(/\D/g, ''),
+    //             data.cep.replace(/\D/g, ''),
+    //             data.address
+    //         ]
 
-        const results = await db.query(query)
+    //         const results = db.query(query, values)
+    //         return (await results).rows[0].id
+    //     }catch(err) {
+    //         console.error(err)
+    //     }
+    // },
+    // async update(id, fields) {
+    //     let query = "UPDATE users SET"
 
-        return results.rows[0]
-    }, 
-    async create(data) {
-        try{
-            const query = `
-            INSERT INTO users (
-                name,
-                email,
-                password, 
-                cpf_cnpj,
-                cep, 
-                address
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id
-            `
-            //hash de senha - criptografia de senha 
-            // instalando a biblioteca bcryptjs
-            const passwordHash = await hash(data.password, 8) //a força da senha sera 8
+    //     Object.keys(fields).map((key, index, array) => {
+    //         if((index + 1) < array.length) {
+    //             query = `${query}
+    //                 ${key} = '${fields[key]}',
+    //             `
+    //         } else {
+    //             query = `${query}
+    //                 ${key} = '${fields[key]}'
+    //                 WHERE id = ${id}
+    //             `
+    //         }
+    //     })
 
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ''),
-                data.cep.replace(/\D/g, ''),
-                data.address
-            ]
+    //     await db.query(query)
+    //     return
+    // },
+    // async delete(id) {
+    //     //pegar todos os produtos do usuario
+    //     let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
+    //     const products = results.rows
 
-            const results = db.query(query, values)
-            return (await results).rows[0].id
-        }catch(err) {
-            console.error(err)
-        }
-    },
-    async update(id, fields) {
-        let query = "UPDATE users SET"
+    //     //dos produtos, pegar todas as imagens/arquivos
+    //     const allFilesPromise = products.map(product =>
+    //         Product.files(product.id))
 
-        Object.keys(fields).map((key, index, array) => {
-            if((index + 1) < array.length) {
-                query = `${query}
-                    ${key} = '${fields[key]}',
-                `
-            } else {
-                query = `${query}
-                    ${key} = '${fields[key]}'
-                    WHERE id = ${id}
-                `
-            }
-        })
+    //     let promiseResults = await Promise.all(allFilesPromise) //array com as promessas resolvidas
 
-        await db.query(query)
-        return
-    },
-    async delete(id) {
-        //pegar todos os produtos do usuario
-        let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
-        const products = results.rows
+    //     //rodar a remocao do usuario 
+    //     await db.query('DELETE FROM users WHERE id = $1', [id])
 
-        //dos produtos, pegar todas as imagens/arquivos
-        const allFilesPromise = products.map(product =>
-            Product.files(product.id))
-
-        let promiseResults = await Promise.all(allFilesPromise) //array com as promessas resolvidas
-
-        //rodar a remocao do usuario 
-        await db.query('DELETE FROM users WHERE id = $1', [id])
-
-        //remover as imagens da pasta public
-        promiseResults.map(results => { //lembrando que é um result para cada produto, ou seja, cada produto com os arquivos
-            results.rows.map(file => {
-                try{
-                    fs.unlinkSync(file.path)
-                }catch(err) {
-                    console.error(err)
-                }
-            })
-        })
-    }
+    //     //remover as imagens da pasta public
+    //     promiseResults.map(results => { //lembrando que é um result para cada produto, ou seja, cada produto com os arquivos
+    //         results.rows.map(file => {
+    //             try{
+    //                 fs.unlinkSync(file.path)
+    //             }catch(err) {
+    //                 console.error(err)
+    //             }
+    //         })
+    //     })
+    // }
 }
+
+console.log(User)
+
+module.exports = User
