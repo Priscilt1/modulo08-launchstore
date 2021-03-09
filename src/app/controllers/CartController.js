b78ed9b46b91857bd4196500b6a49130729246c3
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-09 16:23:19 -0300

    Ultilizando a funcionalidade de adicionar ao carrinho

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -4,16 +4,33 @@ const LoadProductServices = require('../services/LoadProductService')
 module.exports = {
     async index(req, res) {
         try {
-            const product = await LoadProductServices.load('product', { where : {id: 10}})
             let { cart } = req.session
 
             //gerenciador de carrinho
-            cart = Cart.init(cart).addOne(product) //iniciando o carrinho e retornando o objeto vazio
+            cart = Cart.init(cart)//iniciando o carrinho e retornando o objeto vazio
 
             return res.render('cart/index', { cart })
         }    
         catch(err) {
             console.error(err)
         }
+    },
+    async addOne(req, res) {
+        //pegar o id do produto e o produto
+        const { id } = req.params
+        
+        const product = await LoadProductServices.load('product', { where : { id }})
+
+        //pegar o carrinho da sessao 
+        let { cart } = req.session
+
+        //adicionar o produto ao carrinho usando o gerenciador de carrinho
+        cart = Cart.init(cart).addOne(product)
+
+        //atualizar o carrinho da sessao
+        req.session.cart = cart
+
+        //redirecionar o usuario para a tela do carrinho 
+        return res.redirect('/cart')
     }
 }
