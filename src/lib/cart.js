ad7e52b2437b85c86637c745b416926ffe091cf4
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-09 10:59:46 -0300

    adicionando item no carrinho

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -5,7 +5,7 @@ const { formatPrice } = require('./utils')
 const Cart = {
     init(oldCart) {
         if(oldCart) {
-            this.items = oldCart.items
+            this.items = oldCart.items //[{ product: {}. price, quantity, formatted, {} }]
             this.total = oldCart.total
         } else {
             this.items = []
@@ -17,8 +17,40 @@ const Cart = {
         }
         return this
     },
-    addOne(product) {
-    //adicionar 1 item ao carrinho
+    addOne(product) {    //adicionar 1 item ao carrinho
+        //ver se um produto ja existe no carrinho 
+        let inCart = this.items.find(item => item.product.id == product.id)
+
+        //se nao existir 
+        if (!inCart) {
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            }
+
+            this.items.push(inCart)
+        }
+
+        // para nao colocar mais quantidade no carrinho sem ter estoque suficiente
+        if(inCart.quantity >= product.quantity) return this 
+
+        //Atualizando item
+        inCart.quantity++ //atualizando o item no carrinho
+        inCart.price = inCart.product.price * inCart.quantity //multiplicando o preco de acordo com a quantidade
+        inCart.formattedPrice = formatPrice(inCart.price)
+        
+        //Atualizando carrinho
+        this.total.quantity++ //atualizando o carrinho 
+        this.total.price += inCart.product.price //somando o preco no carrinho
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        return this
+
     },
     removeOne(productId) {
     //remover 1 item do carrinho 
@@ -27,5 +59,13 @@ const Cart = {
     //deletar todo item
     }
 }
+
+const product = {
+    id: 1,
+    price: 199,
+    quantity: 2
+}
+
+console.log(Cart.init().addOne(product))
 
 module.exports = Cart
